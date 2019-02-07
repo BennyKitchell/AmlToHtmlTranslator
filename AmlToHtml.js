8643feb1f5da804b9d57b6027471c598d569cf38
Benny Kitchell <bennykitchell@gmail.com>
2019-02-06 19:26:09 -0600

    Create the aml to html function that accepts the relevant elemtnts

--- AmlToHtml.js
+++ AmlToHtml.js
@@ -1,15 +1,39 @@
 var AMLTranslator = (function () {
-	
+	// This will act as a traditional stack data structure
+	// Since JS doesnt support stacks, we will use an array and just push and pop accordingly
+	var elementStack = [];
+
+	// aml dictionary for easier lookup for translating
+	// alternatively we could include another dictionary for other translations
 	var amlDict = {
 		'^': 'openingElement',
 		'!': 'closingElement',
 		'%': 'STRONG',
 		'~': 'EM'
 	};
+
 	// The main function that accepts the test cases
 	var translate = function(input) {
+		var output = [];
+		// Split the input
+		var amlArray = input.split('');
+		// search through the array each character in the string
+		amlArray.forEach( function(element, index) {
+			// get the next element
+			var nextElement = amlArray[index+1];
+			//get the element two down
+			var elementAfterNext = amlArray[index+2];
+
+		});
+		
 		return input;
 	};
+
+	// This function will be used for translating aml to html
+	// Alternatively we could have other functions for different translations
+	var amlToHtml = function(nextElement, elementAfterNext){
+		
+	}
 
 	return { "translate": translate };
 }());
