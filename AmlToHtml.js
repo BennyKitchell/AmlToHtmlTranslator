144e3c8a32999503ede029c025f5c706233cc45f
Benny Kitchell <bennykitchell@gmail.com>
2019-02-06 19:45:29 -0600

    Work out the logic for the stack and how to add and remove it when necessary

--- AmlToHtml.js
+++ AmlToHtml.js
@@ -32,7 +32,28 @@ var AMLTranslator = (function () {
 	// This function will be used for translating aml to html
 	// Alternatively we could have other functions for different translations
 	var amlToHtml = function(nextElement, elementAfterNext){
-		
+		// If the next element is the closing element 
+		if(amlDict[nextElement] === 'closingElement') {
+			var closingElement = amlDict[elementAfterNext];
+			var top = elementStack[0];
+			var index = elementStack.indexOf(closingElement);
+
+			// then check the stack to see if the element is closing an already open one
+			if(elementStack.includes(closingElement)){
+				// if it is close the current element and open the new one 
+				elementStack.splice(index, 1);
+				return `</${top}></${closingElement}><${top}>`;
+			}
+			// otherwise just close it without opening a new one
+			else {
+				elementStack.splice(index, 1);
+				return `</${closingElement}>`;
+			}
+		}
+		else {
+			elementStack.unshift(amlDict[nextElement]);
+			return `<${amlDict[nextElement]}>`;
+		}
 	}
 
 	return { "translate": translate };
