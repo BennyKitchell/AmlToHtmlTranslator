69651769b3117bbdbc22d60e70288c87b7cf139c
Benny Kitchell <bennykitchell@gmail.com>
2019-02-06 19:12:51 -0600

    Include a dictionary for easier lookups of elements

--- AmlToHtml.js
+++ AmlToHtml.js
@@ -1,13 +1,18 @@
 var AMLTranslator = (function () {
 	
+	var amlDict = {
+		'^': 'openingElement',
+		'!': 'closingElement',
+		'%': 'STRONG',
+		'~': 'EM'
+	};
+	// The main function that accepts the test cases
 	var translate = function(input) {
-
 		return input;
 	};
 
 	return { "translate": translate };
 }());
-
 
 if(module.exports) {
 	module.exports = AMLTranslator;
